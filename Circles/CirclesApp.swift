9c216dfbc87b83481579d2f2a3dfcd366000cd80
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-06-22 13:45:17 -0700

    Initial Commit

---
+++ Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by Nathaniel Bedggood on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CirclesTests/CirclesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesTests.swift
+//  CirclesTests
+//
+//  Created by Nathaniel Bedggood on 22/06/2025.
+//
+
+import Testing
+@testable import Circles
+
+struct CirclesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
