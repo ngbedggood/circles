f287a9ce1ca395bf940afd773e5186b3933a6d16
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-06-22 13:50:07 -0700

    Test commit

--- Circles/ContentView.swift
+++ Circles/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Ni Hao, world!")
         }
         .padding()
     }
