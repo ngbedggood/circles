151eb08376fc748e246d859b0736aee290d23c48
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-06-23 17:20:42 -0700

    Vertical and horizontal scrolling between personal and social card views

---
+++ Circles/VerticalPager.swift
@@ -0,0 +1,18 @@
+//
+//  VerticalPager.swift
+//  Circles
+//
+//  Created by Nathaniel Bedggood on 23/06/2025.
+//
+
+import SwiftUI
+
+struct VerticalPager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerticalPager()
+}
