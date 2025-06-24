fc84fbec81ac5fcf989c95d3e84e808bc5c34ea6
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-06-23 17:22:57 -0700

    .

--- Circles/CardData.swift
+++ Circles/CardData.swift
@@ -6,13 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum CardColor {
-    case red, yellow, orange, green, blue
+    case red, yellow, orange, green, blue, none
+    
+    var swiftUIColor: Color {
+            switch self {
+            case .red: return .red
+            case .yellow: return .yellow
+            case .orange: return .orange
+            case .green: return .green
+            case .blue: return .blue
+            case .none: return .gray
+            }
+        }
 }
 
 struct PersonalCard: Identifiable {
     let id = UUID()
     var date: String
     var color: CardColor?  // can be nil
+}
+
+struct FriendColor: Identifiable {
+    let id = UUID()
+    var name: String
+    var color: CardColor?
+}
+
+struct SocialCard: Identifiable {
+    let id = UUID()
+    var date: String
+    var friends: [FriendColor]
 }
--- Circles/ContentView.swift
+++ Circles/ContentView.swift
@@ -9,37 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var currentOffset: CGFloat = 0
-    @GestureState private var dragOffset: CGFloat = 0
-    @State private var dayOffset: Int = 0  // Number of days from today
-    
     let date = Date()
 
     //Dummy data
-    @State private var cards = [
-        CardData(date: "20th June 2025", color: nil),
-        CardData(date: "21st June 2025", color: nil),
-        CardData(date: "22nd June 2025", color: nil),
-        CardData(date: "23rd June 2025", color: nil),
-        CardData(date: "24th June 2025", color: nil),
+    @State private var personalCards = [
+        PersonalCard(date: "20th June 2025", color: nil),
+        PersonalCard(date: "21st June 2025", color: nil),
+        PersonalCard(date: "22nd June 2025", color: nil),
+        PersonalCard(date: "23rd June 2025", color: nil),
+        PersonalCard(date: "24th June 2025", color: nil),
+    ]
+    var socialCards = [
+        SocialCard(date: "20th June 2025", friends: [FriendColor(name: "Jack", color: .blue), FriendColor(name: "John", color: .yellow), FriendColor(name: "Mary", color: .yellow)]),
+        SocialCard(date: "20th June 2025", friends: [FriendColor(name: "Jack", color: .green), FriendColor(name: "John", color: .red), FriendColor(name: "Mary", color: .red)]),
+        SocialCard(date: "20th June 2025", friends: [FriendColor(name: "Jack", color: .green), FriendColor(name: "John", color: .orange), FriendColor(name: "Mary", color: .red)]),
+        SocialCard(date: "20th June 2025", friends: [FriendColor(name: "Jack", color: .yellow), FriendColor(name: "John", color: .yellow), FriendColor(name: "Mary", color: .green)]),
+        SocialCard(date: "20th June 2025", friends: [FriendColor(name: "Jack", color: .blue), FriendColor(name: "John", color: .yellow), FriendColor(name: "Mary", color: .blue)]),
     ]
     
-    @State private var selection = 0
+    @State private var horizontalIndex = 0
+    @State private var verticalIndex = 0
     
     var body: some View {
-        
-        TabView(selection: $selection) {
-            ForEach(cards.indices, id: \.self) { index in
-                    CardView(card: $cards[index])
-                        .frame(height: 740)
-                        .background(RoundedRectangle(cornerRadius: 20).fill(Color.white).shadow(radius: 10))
-                        .padding(40)
+        ZStack {
+            TabView(selection: $horizontalIndex) {
+                ForEach(personalCards.indices, id: \.self) { index in
+                    VerticalPager(personalCard: $personalCards[index], socialCard: socialCards[index], verticalIndex: $verticalIndex)
                         .tag(index)
                 }
             }
-        .tabViewStyle(.page(indexDisplayMode: .never)) //snap pages
-        .onAppear {
-            selection = cards.count - 1
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .onAppear {
+                horizontalIndex = personalCards.count - 1
+            }
+            .gesture(verticalIndex == 0 ? DragGesture() : nil)
         }
     }
 }
