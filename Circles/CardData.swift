71a39aa74fe852e5cbcb72b75534773680eeb280
Daniel Bedggood <ddbedggood@gmail.com>
2025-06-25 20:10:30 +1200

    Test precommit hook from terminal

--- Circles/CardData.swift
+++ Circles/CardData.swift
@@ -13,18 +13,13 @@ enum CardColor {
 
     var swiftUIColor: Color {
         switch self {
-        case .gray: return .gray
-        case .yellow: return .yellow
-        case .orange: return .orange
-        case .green: return .green
-        case .teal: return .teal
-        case .none: return .gray
-        
-            
-            
-            
-            
-            
+            case .gray: return .gray
+            case .yellow: return .yellow
+            case .orange: return .orange
+            case .green: return .green
+            case .teal: return .teal
+            case .none: return .gray
+
         }
     }
 }
