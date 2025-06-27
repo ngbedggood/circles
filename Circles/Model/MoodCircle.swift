b17ddc791aa51d5ed35d189dd86a9ec1a90b2c54
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-06-27 16:44:14 -0700

    Mood circles populated via loop instead of hard coded

---
+++ Circles/Model/MoodCircle.swift
@@ -0,0 +1,20 @@
+//
+//  MoodCircle.swift
+//  Circles
+//
+//  Created by Nathaniel Bedggood on 27/06/2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct MoodCircle {
+    let color: MoodColor
+    let fill: Color
+    let offsetY: CGFloat
+    let expandedSize: CGFloat
+    let defaultSize: CGFloat
+    let index: Int
+}
+
+
